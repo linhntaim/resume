54e0b333081293105639b3f3444dd9a8567a7d84
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2018-12-25 18:06:25 +0700

    Update vi.php

--- vi.php
+++ vi.php
@@ -254,7 +254,7 @@
                         <ul>
                             <li>Internet Information Services (IIS).</li>
                             <li>Apache.</li>
-                            <li>Nginx.</li>
+                            <li>NGINX.</li>
                         </ul>
                     </li>
                     <li>
@@ -281,7 +281,7 @@
                             <li>JetBrains PhpStorm, Netbeans, Eclipse, Notepad++, console/terminal.</li>
                             <li>Xcode, Android Studio.</li>
                             <li>Adobe Animate (formerly Adobe Flash Professional), Adobe Flash Builder, Brackets.</li>
-                            <li>Git, JIRA, Trello, Jenkins.</li>
+                            <li>Git(hub/lab), JIRA, Trello, Jenkins.</li>
                         </ul>
                     </li>
                     <li>
